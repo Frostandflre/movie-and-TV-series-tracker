7bb21e366f00720109f921ae5b7d2ff9d7dc0b0b
Frostandflre <mineciler555@gmail.com>
2025-01-29 17:42:15 +0300

    Add documentation and remove folder app_root_dir

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from .extensions import database
+
+class Users(database.Model):
+    user_id = database.Column(database.Integer,primary_key=True)
+    nickname = database.Column(database.String(64), unique=True, index=True)
+    password = database.Column(database.String(512))
+    email = database.Column(database.String(120), unique=True, index=True)
+    is_admin = database.Column(database.Boolean, default=False)
